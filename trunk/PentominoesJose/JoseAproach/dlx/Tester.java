855ec12236eb4ad0f798bbbb4eec4009beec268d
jose.sue.pty <jose.sue.pty@185c90ea-b011-11de-b5cd-01dc870c4969>
2009-10-07 17:28:40 +0000

    try-out initial approach(no solver yet)

---
+++ trunk/PentominoesJose/JoseAproach/dlx/Tester.java
@@ -0,0 +1,20 @@
+package dlx;
+
+import mio.Board;
+
+public class Tester 
+{
+	public static void main (String[] args)
+	{
+		Board boardy = new Board();
+		//System.out.print(boardy.get(0, 0));
+		boardy.set(0, 0, 0, "L"); // state which is the next empty box(in this case we take 0,0) third value is the pentominoe to represent
+		//boardy.sety(0, 0, 0,"L");
+		//boardy.set1(0, 0, 0, "L");
+		//System.out.print(boardy.get(0, 3));
+		System.out.println(boardy);
+		//System.out.println(boardy.get(0, 4));
+		
+	}
+
+}
