ff03597b5ed1a0401fe35c8238d5e2a239743d6b
maarfijs <maarfijs@185c90ea-b011-11de-b5cd-01dc870c4969>
2009-10-08 23:51:06 +0000

    This thing generates all the possible permutations. It even does it properly, hooray!

--- trunk/src/Stepper.java
+++ trunk/src/Stepper.java
@@ -1,50 +1,104 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Stepper {
 	
 	public static void main(String[] args) {
 		Stepper stepper = new Stepper();
-		Scanner in = new Scanner(System.in);
-		boolean stop = false;
-		do {
-			System.out.println(stepper.toString());
+//		Scanner in = new Scanner(System.in);
+//		boolean stop = false;
+		int i = 1;
+		while(!stepper.walked) {
 			stepper.step();
-			if (in.next() == "q") {
-				stop = true;
-			}
-		} while(!stop);
+			i++;
+//			System.out.println(i++ + "	" + stepper.toString());
+//			try {
+//				Thread.sleep(500);
+//			} catch (Exception e) {
+//				break;
+//			}
+		}
+		System.out.println(i);
 	}
 	
 	public Stepper() {
 		this(6, 12);
 	}
 	
-	public Stepper(int countOfLevels, int countOfChoices) {
-		choices = countOfChoices;
-		levels = countOfLevels;
+	public Stepper(int numberOfLevels, int numberOfChoices) {
+		choices = numberOfChoices;
+		levels = numberOfLevels;
 		map = new int[levels];
 	}
 	
 	public void step() {
-		if (map[levels - 1] == choices - 1) {
-			map[levels - 1] = 0;
-			for (int i = 0; i < levels - 1; i++){
-				if (map[levels - 2 - i] == choices - 1) {
-					map[levels - 2 - i] = 0;
-				} else {
-					map[levels - 2 - i]++;
+		if(walked) // If already done
+			return;
+		
+		// If the tree is empty, fill it with fresh fruits!!!1
+		if (map[0] == 0) {
+			for (int i = 0; i < levels; i++) {
+				map[i] = i + 1;
+			}
+			return;
+		}
+		
+		// Lets find the next available value for the last level
+		for (int i = 0; i < choices - map[levels - 1]; i++) {
+			if (isAvailable(levels, map[levels - 1] + 1 + i)) {
+				map[levels - 1] = map[levels - 1] + 1 + i;
+				return;
+			}
+		}
+
+		// So there are no more possible values for the last one, so we need to increment something
+		int firstNiceLevel = 0;
+		branchSearch:
+		for (int i = 0; i < levels - 1; i++) {
+			for (int j = 0; j < choices - map[levels - 2 - i]; j++) {
+				if (isAvailable(levels - 1 - i, map[levels - 2 - i] + 1 + j)) {
+					firstNiceLevel = levels - 1 - i;
+					map[levels - 2 - i] = map[levels - 2 - i] + 1 + j;
+					break branchSearch;
+				}
+			}
+			// So this level have no more possible values, mark it for that shame!
+			map[levels - 2 - i] = 0;
+		}
+		
+		// In case we try to take another round
+		if(map[0] == 0) {
+			walked = true;
+			return;
+		}
+		
+		// Lets fill the blank spots
+		int lastNewValue = 0;
+		for (int i = 0; i < levels - firstNiceLevel; i++) {
+			for (int j = lastNewValue + 1; j < choices + 1; j++) {
+				if (isAvailable(firstNiceLevel + 1 + i, j)) {
+					map[firstNiceLevel + i] = j;
+					lastNewValue = j;
 					break;
 				}
 			}
-		} else {
-			map[levels - 1]++;
 		}
 	}
 	
-	public String toString() {
-		return new String(map[0] + " " + map[1] + " " + map[2] + " " + map[3] + " " + map[4] + " " + map[5]);
+	public boolean isAvailable(int level, int choice) {
+		for (int i = 0; i < level - 1; i++) {
+			if (map[i] == choice) {
+				return false;
+			}
+		}
+		return true;
 	}
 	
+	public String toString() {
+		// this is the WRONG way :)
+		return new String(map[0] + "	" + map[1] + "	" + map[2] + "	" + map[3] + "	" + map[4] + "	" + map[5]);
+	}
+	
+	private boolean walked = false;
 	private int choices;
 	private int levels;
 	private int[] map;
