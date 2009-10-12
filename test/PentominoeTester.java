fdddc56f883c288315ab1624f6f7a137c6f42500
ronald.gerits <ronald.gerits@185c90ea-b011-11de-b5cd-01dc870c4969>
2009-10-12 17:57:50 +0000

    Updated Testing Classes: PentominoeTester and BoardTester.. Up till now it only tests the valid options...

--- test/PentominoeTester.java
+++ test/PentominoeTester.java
@@ -1,37 +1,40 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-import java.util.ArrayList;
-
-/**
- *
- * @author Beheerder
- */
-public class PentominoeTester {
-
-    public static void main(String[] args)
+public class PentominoeTester
+{
+     /** This constructor inputs all the Pentominodata and stores it in an array.
+      *
+      *
+      */
+    public PentominoeTester()
     {
-        Pentomino I = new Pentomino("#####\n");
-        Pentomino L = new Pentomino("####\n...#\n");
-        Pentomino P = new Pentomino("##.\n##.\n#.\n");
-        Pentomino fout = new Pentomino("##\n###\n");
+        Pentomino pentI = new Pentomino("#####\n"); //1
+        Pentomino pentL = new Pentomino("####\n...#\n"); //2
+        Pentomino pentP = new Pentomino("###\n##.\n"); //3
+        Pentomino pentX = new Pentomino(".#.\n###\n.#.\n"); //4
+        Pentomino pentF = new Pentomino(".##\n##.\n.#.\n"); //5
+        Pentomino pentN = new Pentomino("###.\n..##\n"); //6
+        Pentomino pentT = new Pentomino("###\n.#.\n.#.\n"); //7
+        Pentomino pentU = new Pentomino("###\n#.#\n"); //8
+        Pentomino pentV = new Pentomino("###\n#..\n#..\n"); //9
+        Pentomino pentY = new Pentomino("####\n..#.\n"); //10
+        Pentomino pentZ = new Pentomino("##.\n.#.\.##\n"); //11
+        Pentomino pentW = new Pentomino(".##\n##.\n#..\n"); //12
 
-        Pentomino[] array = new Pentomino[11];
-        array[0] = I;
-        array[1] = L;
-        array[2] = P;
-        array[3] = fout;
+        pentominoArray = new Pentomino[12];
+        pentominoArray = {pentI, pentL, pentP, pentX, pentF, pentN, pentP, pentT, pentU, pentV, pentY, pentZ, pentW};
 
-        for (Pentomino pentominoes : array)
+        for(Pentomino pent : pentominoArray)
         {
-            System.out.println("Pentominoe: \n" + pentominoes.toString());
-
-        for (Object mutations : pentominoes.getMutations())
-        {
-            System.out.println(mutations.toString());
-        }
+            System.out.println("Basic Pentomino: " + pent.toString());
+            for(Object mutation : pent.generateMutations())
+            {
+                System.out.println("Mutation: " + mutation.toString());
+            }
         }
     }
+    public Pentomino[] returnPentominos()
+    {
+        return pentominoArray;
+    }
+
+    private Pentomino[] pentominoArray;
 }
