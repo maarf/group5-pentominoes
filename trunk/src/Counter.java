d1be30812bda5c7ada7bb87852067a96ae925801
haagmansleoni09@gmail.com <haagmansleoni09@gmail.com@185c90ea-b011-11de-b5cd-01dc870c4969>
2009-11-10 13:26:18 +0000

    Ronald and I worked on our classes

---
+++ trunk/src/Counter.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Leoni
+ */
+public class Counter
+{
+    /**
+     * Method that makes every new pentomino move one line every step in time.
+     * @param new
+     */
+    public void movePentomino(Pentomino nieuw)
+    {
+        
+    }
+
+
+}
