f5cd35adf49279b3d30184ea447b175122ad9dc6
haagmansleoni09@gmail.com <haagmansleoni09@gmail.com@185c90ea-b011-11de-b5cd-01dc870c4969>
2009-11-10 13:28:02 +0000

    Ronald and I worked on our classes

--- src/Board_1.java
+++ src/Board_1.java
@@ -7,7 +7,7 @@
 
 /**
  *
- * @author Leoni
+ * @author Leoni Haagmans & Ronald Gerits
  */
 
 import javax.swing.*;
@@ -15,29 +15,47 @@ import javax.swing.*;
 public class Board_1 extends JComponent
 {
      public static void main(String[] args)
-    {
+     {
         JFrame frame = new JFrame();
         JButton button = new JButton("Play");
         JButton button2 = new JButton("Pause");
 
-        //final JLabel label = new JLabel();
         Board2 bord = new Board2();
 
         JPanel panel = new JPanel();
 
         panel.add(button);
         panel.add(button2);
-        //panel.add(label);
-        //panel.add(bord);
         frame.add(panel);
       
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setTitle("Pentris");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-    }
+     }
 
-    private static final int FRAME_WIDTH = 400;
-    private static final int FRAME_HEIGHT = 400;
+     /**This method returns the height
+      *
+      * @return int
+      */
+     public int getHeight()
+     {
+         return height;
+     }
+
+     /** Returns width
+      *
+      * @return int
+      */
+     public int getWidth()
+     {
+         return width;
+     }
+
+     private int width;
+     private int height;
+     public Pentomino NextPent;
+     private static final int FRAME_WIDTH = 400;
+     private static final int FRAME_HEIGHT = 400;
 }
 
