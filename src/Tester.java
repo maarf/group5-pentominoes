468204048a43989e5aad69314b0f87bb6487c0b5
jose.sue.pty <jose.sue.pty@185c90ea-b011-11de-b5cd-01dc870c4969>
2010-01-11 13:57:06 +0000

    bruteforce updated

--- src/Tester.java
+++ src/Tester.java
@@ -3,15 +3,19 @@ public class Tester
 {
 	public static void main(String[]args)
 	{
-		RandParcelSelector selector = new RandParcelSelector();
-		Parcel aParcel = new Parcel(selector.getParcelX(),selector.getParcelY(),selector.getParcelZ(),selector.getParcelV());
-		System.out.println("Actual value: " + aParcel.getValue());
-		System.out.println("X: " + aParcel.getParcelX());
-		System.out.println("Y: " + aParcel.getParcelY());
-		System.out.println("Z: " + aParcel.getParcelZ());
-		Truck truck = new Truck(4,4,4);//33,5,8
-		truck.setParcel(aParcel);
-		truck.getTruckValue();
+		//RandParcelSelector selector = new RandParcelSelector();
+	//	Parcel aParcel = new Parcel(selector.getParcelX(),selector.getParcelY(),selector.getParcelZ(),selector.getParcelV());
+	//	System.out.println("Actual value: " + aParcel.getValue());
+	//	System.out.println("X: " + aParcel.getParcelX());
+	//	System.out.println("Y: " + aParcel.getParcelY());
+	//	System.out.println("Z: " + aParcel.getParcelZ());
+		//Truck truck = new Truck(4,4,4);//33,5,8
+		//truck.setParcel(aParcel);
+	//	truck.getTruckValue();
 		//System.out.println("Value: " + truck.getTruckValue());
+		
+		Truck truck = new Truck(33,5,8);
+		BruteForce1 solver1 = new BruteForce1(truck);
+		solver1.Solve();
 	}
 }
