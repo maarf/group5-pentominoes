98f8ef6b035598d9df7c23d469289055a41d5cb9
ronald.gerits@gmail.com <ronald.gerits@gmail.com@185c90ea-b011-11de-b5cd-01dc870c4969>
2010-01-11 13:15:03 +0000

    Updated the greedy-algorithm

--- src/Greedy.java
+++ src/Greedy.java
@@ -1,17 +1,46 @@
 public class Greedy
 {
-	public Greedy(Truck aTruck)
+	public Greedy(Truck aTruck, Parcel[] parcels)
 	{
 		aTruck = truck;
+		this.parcels = parcels;
+		makeGreedy();
 		Solve();
 	}
 	
-	public void Solve()
+	public void makeGreedy()
 	{
-		
+		// here the program should sort the parcels into descending order (highest value first).
+		Parcel maxParcel = new Parcel(0,0,0,0);
+		for(int i = 0; i < parcels.length; i++)
+		{
+			if(parcels[i].getValue() > maxParcel.getValue()) maxParcel = parcels[i];
+		}
+		greedyParcels = new Parcel[parcels.length];
+		greedyParcels[0] = maxParcel;
+		for(int i = 1; i < parcels.length - 1; i++)
+		{
+			for(int j = maxParcel.getValue(); j > 0; j--)
+			{
+				
+			}
+		}
+	}
+	
+	public boolean Solve()
+	{
+		for(int i = 0; i < greedyParcels.length; i++)
+		{
+			Parcel parcel = greedyParcels[i];
+			if(truck.fits(parcel) == true) truck.setParcel(parcel);
+		}
+		if(truck.isFull() == true) return true;
+		else return false;
 	}
 	
 	private Truck truck;
+	private Parcel[] parcels;
+	private Parcel[] greedyParcels;
 	
 	
 }
