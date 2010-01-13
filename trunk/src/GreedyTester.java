7e7dc89d7aada2716e9ebb88caf36404435c7736
ronald.gerits@gmail.com <ronald.gerits@gmail.com@185c90ea-b011-11de-b5cd-01dc870c4969>
2010-01-13 11:27:11 +0000

    a

--- trunk/src/GreedyTester.java
+++ trunk/src/GreedyTester.java
@@ -2,18 +2,28 @@ public class GreedyTester
 {
 	public static void main(String[] args)
 	{
-		Parcel parcelA = new Parcel(2,4,1,3);
-		Parcel parcelB = new Parcel(2,4,4,4);
+		Parcel parcelA = new Parcel(2,2,4,3);
+		Parcel parcelB = new Parcel(2,3,4,4);
 		Parcel parcelC = new Parcel(3,3,3,5);
 		
 		Parcel[] parcels = {parcelA, parcelB, parcelC};
 		
-		Truck truck = new Truck(33,6,2);
+		Truck truck = new Truck(8,5,33);
 		
 		Greedy greedy = new Greedy(truck, parcels);
 		
-		greedy.makeGreedy();
-		greedy.Solve();
+		Parcel[] greedyParcels = greedy.makeGreedy();
+		
+		for(int i = 0; i < greedyParcels.length; i++)
+		{
+			System.out.println(greedyParcels[i].toString());
+		}
+
+		truck.setParcel(parcelA);
+		truck.setParcel(parcelB);
+		truck.setParcel(parcelC);
+		truck.getTruckValue();
+		//greedy.Solve();
 		
 	}
 }
