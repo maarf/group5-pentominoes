3314b357a5b61c512ce7cd1e07b5f0ec3265b9fb
ronald.gerits <ronald.gerits@185c90ea-b011-11de-b5cd-01dc870c4969>
2010-01-13 09:33:20 +0000

    44

--- src/GreedyTester.java
+++ src/GreedyTester.java
@@ -2,13 +2,13 @@ public class GreedyTester
 {
 	public static void main(String[] args)
 	{
-		Parcel parcelA = new Parcel();
-		Parcel parcelB = new Parcel();
-		Parcel parcelC = new Parcel();
+		Parcel parcelA = new Parcel(2,4,1,3);
+		Parcel parcelB = new Parcel(2,4,4,4);
+		Parcel parcelC = new Parcel(3,3,3,5);
 		
 		Parcel[] parcels = {parcelA, parcelB, parcelC};
 		
-		Truck truck = new Truck();
+		Truck truck = new Truck(33,6,2);
 		
 		Greedy greedy = new Greedy(truck, parcels);
 		
