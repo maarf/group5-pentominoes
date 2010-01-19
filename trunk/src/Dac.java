2926b509072ca6da87fab382af431aab057f9503
jose.sue.pty <jose.sue.pty@185c90ea-b011-11de-b5cd-01dc870c4969>
2010-01-19 21:03:30 +0000

    first divide and conquer

--- trunk/src/Dac.java
+++ trunk/src/Dac.java
@@ -13,6 +13,7 @@ public class Dac
 	private int [][] parcels;
 	private int [][][] smallTruck;
 	private int [][][] newTruck;
+	private int currX = 0, currY = 0, currZ = 0;
 	
 	public Dac(Truck aTruck, int [][] a)
 	{
@@ -28,9 +29,25 @@ public class Dac
 		solver.Solve(parcels);
 		smallTruck = bTruck.getTruck();
 		newTruck = new int[truck.getHeight()][truck.getLength()][truck.getWidth()];
-		for(int i = 0; i<11; i++)
+		for(int s = 0; s<11; s++)
 		{
-			
+			concatenate();
+			for(int i = 0; i < newTruck.length -1; i++)
+			{
+				for(int j = 0; j < newTruck[i].length-1; j++)
+				{
+					for(int k = 0; k < newTruck[i][j].length-1; k++)//msschn niet goed
+					{
+						if(!isfilled(i,j,k)) 
+						{
+							currY = i;
+							currZ = j;
+							currX = k;
+							return;
+						}					
+					}		
+				}		
+			}
 		}
 		
 	}
@@ -43,12 +60,35 @@ public class Dac
 			{
 				for(int k = 0; k < newTruck[i][j].length; k++)
 				{
-					newTruck[i][j][k] = smallTruck [i][j][k];				
+					newTruck[currY+i][currZ+j][currX+k] = smallTruck [i][j][k];				
 				}
 			}
 		}
 	}
 
+	public boolean isfilled(int y, int z, int x)
+	{
+        if (newTruck[y][z][x] == 0) 
+        {
+            return false;
+        }
+        return true;
+	}
 	
+	public void getTruckValue()
+	{
+		for(int i = 0; i < newTruck.length; i++)
+		{
+			for(int j = 0; j < newTruck[i].length; j++)
+			{
+				for(int k = 0; k < newTruck[i][j].length; k++)
+		        {
+		               System.out.print(newTruck[i][j][k]);   
+		        }
+		         System.out.println();
+		    }
+		    System.out.println();
+	}
 	
+	}
 }
---
+++ trunk/src/DacTester.java
@@ -0,0 +1,27 @@
+
+public class DacTester 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		Truck truck = new Truck(8,33,5);
+		
+		Dac solver = new Dac(truck,parcels);
+		solver.solver();
+		solver.getTruckValue();
+		
+
+	}
+	private final static int aX = 2, aY = 2, aZ = 4, aV = 3;
+	private final static int bX = 2, bY = 3, bZ = 4, bV = 4;
+	private final static int cX = 3, cY = 3, cZ = 3, cV = 5;
+	
+	private static int[][] parcels = {	{aX,aY,aZ,aV},
+										{bX,bY,bZ,bV},
+										{cX,cY,cZ,cV} };
+
+
+}
