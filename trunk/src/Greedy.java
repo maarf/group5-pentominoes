6b2c23b46f7877208e08d6f95a51dca7bea0c784
ronald.gerits@gmail.com <ronald.gerits@gmail.com@185c90ea-b011-11de-b5cd-01dc870c4969>
2010-01-20 12:01:18 +0000

    adapted greedy

--- trunk/src/Greedy.java
+++ trunk/src/Greedy.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Greedy
 {
 	public Greedy(Truck aTruck, Parcel[] parcels)
@@ -5,51 +7,20 @@ public class Greedy
 		this.truck = aTruck;
 		this.parcels = parcels;
 		makeGreedy();
-		//Solve();
+		Solve();
 	}
 	
 	public Parcel[] makeGreedy()
 	{
-		// here the program should sort the parcels into ascending order (lowest value first).
-		int max = 0;
-		int numberofParcels = 0;
-		for(int i = 0; i < parcels.length; i++)
-		{
-			if(parcels[i].getValue() > max) max = parcels[i].getValue();
-		}
+		Arrays.sort(parcels);
+		greedyParcels = parcels;
 		
-		
-		Parcel[] temporaryParcels = new Parcel[max + 1];
-		for(int i = 0; i < parcels.length; i++)
-		{
-			for(int j = 1; j <= temporaryParcels.length; j++)
-			{
-				if(parcels[i].getValue() == j) 
-					{
-						temporaryParcels[j] = parcels[i];
-						numberofParcels++;
-					}
-			}
-		}
-		
-		greedyParcels = new Parcel[numberofParcels];
-		int j = 0;
-		for(int i = 0; i < temporaryParcels.length; i++)
-		{
-			if(temporaryParcels[i] != null)
-				{
-					greedyParcels[j] = temporaryParcels[i];
-					j++;
-				}
-			
-		}
-		
-		return greedyParcels;
-		
+		return greedyParcels;		
 	}
 	
 	public Truck Solve()
 	{
+		
 		System.out.println("START");
 		int sets = 0;
 		int nodes = 0;
